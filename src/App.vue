e790dafd67c555b2286a433be1670d21c2a62cf1
luckasdc <mail@luckasdc.be>
2020-10-16 21:19:19 +0200

    text update

--- src/App.vue
+++ src/App.vue
@@ -22,20 +22,21 @@
     <section class="section pt-0" >
       <div class="container has-text-left" style="max-width: 810px">
         <div class="content px-5">
-          <p>Dit jaar ziet onze vlaaienslag er echter iets anders uit, maar de taarten zijn even lekker! Je kan ze op deze website of telefonisch bestellen, en wij brengen ze réchtstreeks tot aan je deur. </p>
+          <p>Welkom bij de <b>jaarlijkse vlaaienslag </b> van Scouts Hellegat! Elk jaar gaan we deur aan deur met onze leden en verkopen we heerlijke limburgse vlaaien. Dit jaar zal het er een beetje anders uitzien, maar geen paniek: de scouts zal u zeker van uw hoogverwachte jaarlijkse taart voorzien.</p>
 
-          <p>Wij leveren je taarten <b>aan huis</b> op <b>zaterdag 14 november </b>, of jij komt ze bij ons ophalen tussen 10u en 15u! Bij vragen can je ons steeds contacteren op <a href="mailto:vlaaienslag@scoutshellegat.be">vlaaienslag@scoutshellegat.be</a>.</p>
+          <p>Dit jaar leveren wij  je taarten <b>aan huis</b> op <b>zaterdag 14 november </b>, of kom je ze die dag aan onze scoutslokalen ophalen tussen 10u en 15u!</p>
+          <p>Omdat we dit jaar onze taarten via een online platform verkopen, wijn we genoodzaakt een servicekost van 0.60 euro te vragen. Hiermee laten we de website waar u nu naar kijkt vlotjes draaien en krijgt u na het afrekenen een mailtje in uw mailbox waarop alle details van uw bestelling terug te vinden zijn. Bij vragen kan je ons steeds contacteren op <a href="mailto:vlaaienslag@scoutshellegat.be">vlaaienslag@scoutshellegat.be</a>. Bedankt voor uw begrip.</p>
+
 
         </div>
-         <div class="pretix-widget-compat has-text-left" event="https://tickets.scoutshellegat.be/vlaaienslag/">
-            <noscript>
-               <div class="pretix-widget">
-                    <div class="pretix-widget-info-message">
-                        JavaScript is disabled in your browser. To access our ticket shop without JavaScript, please <a target="_blank" rel="noopener" href="https://tickets.scoutshellegat.be/vlaaienslag/">click here</a>.
-                    </div>
+        <div class="pretix-widget-compat" event="https://tickets.scoutshellegat.be/vlaaienslag/"> </div>
+        <noscript>
+           <div class="pretix-widget">
+                <div class="pretix-widget-info-message">
+                    JavaScript is disabled in your browser. To access our ticket shop without JavaScript, please <a target="_blank" rel="noopener" href="https://tickets.scoutshellegat.be/vlaaienslag/">click here</a>.
                 </div>
-            </noscript>
-          </div>
+            </div>
+        </noscript>
    </div>
   </section>
 
