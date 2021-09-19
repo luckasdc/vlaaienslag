0ffee1e36b95399fcbf3321bf39f3298c700490f
luckasdc <mail@luckasdc.be>
2021-09-19 09:44:25 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -24,7 +24,7 @@
         <div class="content has-text-left px-5">
           <p>Welkom bij de <b>jaarlijkse vlaaienslag</b> van Scouts Hellegat! Op 9 oktober 2021 zullen wij jullie opnieuw voorzien van heerlijke Limburgse Vlaaien.</p>
           <p>Woon je in Niel? Dan komen we langs bij jouw aan de deur. Woon je elders of wil je zeker zijn van je bestelling, dan kan je hier je vlaaien op voorhand bestellen. Leveren zal gebeuren op 9 oktober tussen 10u en 13u.</p>
-   
+           <p>
             <b>Levering</b> is mogelijk in:
             <b-taglist class="py-2 mb-0">
               <b-tag type="is-primary">Niel</b-tag>
